f30db78b186c30ddd91e968bfe90f78c5c48a23d
JawadMadani <jawad.k.madani@gmail.com>
2018-06-06 16:45:11 +0100

    added BikeService for repository implementation

--- src/main/java/com/globalmatics/bike/models/Bike.java
+++ src/main/java/com/globalmatics/bike/models/Bike.java
@@ -1,19 +1,15 @@
 package com.globalmatics.bike.models;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Date;
 
 @Entity
+@Table(name = "Bikes")
 @Getter
 @Setter
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})

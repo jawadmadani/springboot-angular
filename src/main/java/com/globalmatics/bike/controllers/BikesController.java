646073ea8a86c643dd193366be59de1c1b6cfb1a
JawadMadani <jawad.k.madani@gmail.com>
2018-06-06 16:52:13 +0100

    added BikeService for repository implementation

--- src/main/java/com/globalmatics/bike/controllers/BikesController.java
+++ src/main/java/com/globalmatics/bike/controllers/BikesController.java
@@ -1,15 +1,12 @@
 package com.globalmatics.bike.controllers;
 
 import com.globalmatics.bike.models.Bike;
-import com.globalmatics.bike.repository.BikeRepository;
 import com.globalmatics.bike.service.BikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
